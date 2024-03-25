1cbdb441c9eb505cad1569f3e0ff3f0136b785ee
the-anas-chahdi <anas.chahdi@etu.univ-lyon1.fr>
2024-03-25 14:20:41 +0100

    Début de l'implémentation de la classe Player qui représente notre caractère principal.

---
+++ src/Player.cpp
@@ -0,0 +1,14 @@
+//
+// Created by anasc on 25/03/2024.
+//
+
+#include "Player.h"
+class Player{
+private:
+    int posX, posY;
+public:
+    Player(){
+        posX=0;
+        posY=0;
+    }
+};
---
+++ src/Player.h
@@ -0,0 +1,14 @@
+//
+// Created by anasc on 25/03/2024.
+//
+
+#ifndef LIMITLESSRUNNER_PLAYER_H
+#define LIMITLESSRUNNER_PLAYER_H
+
+
+class Player {
+
+};
+
+
+#endif //LIMITLESSRUNNER_PLAYER_H
