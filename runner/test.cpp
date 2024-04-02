4e1308aa51bbe7b2275fcffb4bb80a131aa1bbee
lounada <damiyineloubn@gmail.com>
2024-04-02 15:35:28 +0200

    test

---
+++ runner/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by etulyon1 on 02/04/2024.
+//
+
+#include "test.h"
---
+++ runner/test.h
@@ -0,0 +1,14 @@
+//
+// Created by etulyon1 on 02/04/2024.
+//
+
+#ifndef LIMITLESSRUNNER_TEST_H
+#define LIMITLESSRUNNER_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //LIMITLESSRUNNER_TEST_H
